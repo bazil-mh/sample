2789d33216daf76d55e45a6912c6949daa55e4ce
bazil-mh <bazil@codesvera.com>
2021-11-25 11:50:59 +0530

    ml sample

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import math
+import random
+
+def sample_data(m,n,s):
+
+    random.seed(s)
+    i=0
+    out_list = []
+    while i < m:
+        j=0
+        list = []
+        while j < n:
+            num = random.random()
+            list.append(num)
+            j+=1
+        out_list.append(list)
+        i+=1
+
+    return out_list
+
+
+
 
 
 
@@ -21,8 +42,12 @@ def index(request):
     list1 = [3, 4, 5, 6]
     list2 = [3, 4, 4, 4]
 
+
+
     #list2[2] = 5
 
-    out_var = dist(list1, list2)
+    dat = sample_data(50,5,6)
+
+    out_var = dist(dat[0], dat[1])
     return render(request, 'app1/index.html', {'out_var': out_var})
 
